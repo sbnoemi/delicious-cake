cc29367f6b44ae45aac3daa571b07cd40d82fd8e
Mike Urbanski <michael.c.urbanski@gmail.com>
2012-11-28 15:15:08 -0500

    [delicious-cake] Version bump for PyPi metadata update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def README():
 
 setup(
     name='delicious-cake',
-    version='0.0.1',
+    version='0.0.2',
     description='A flexible REST framework for Django',
     author='Mike Urbanski',
     author_email='mike@theitemshoppe.com',
